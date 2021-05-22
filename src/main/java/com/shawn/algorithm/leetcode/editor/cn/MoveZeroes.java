b737e0799bdb975327c99a634d43c0be12b0a5fb
shawn <543503245@qq.com>
2021-05-22 21:30:51 +0800

    新增滚雪球方式moreZores

--- src/main/java/com/shawn/algorithm/leetcode/editor/cn/MoveZeroes.java
+++ src/main/java/com/shawn/algorithm/leetcode/editor/cn/MoveZeroes.java
@@ -44,6 +44,18 @@ public class MoveZeroes {
             }
         }
 
+        public void moveZeroesSnowBall(int[] nums) {
+            int snowBallSize = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    snowBallSize++;
+                } else if (snowBallSize > 0) {
+                    nums[i - snowBallSize] = nums[i];
+                    nums[i] = 0;
+                }
+            }
+        }
+
         public void myMoveZeroes(int[] nums) {
             int index = 0;
             for (int i = 0; i < nums.length; i++) {
